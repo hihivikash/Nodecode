1f61321fddc1e25f14f7e74b2f72b5ff9e5f6b02
Vikash <vikash.kumar@soprasteria.com>
2020-06-11 18:32:17 +0530

    changed the naming convention

--- routes/userprofile.js
+++ routes/userprofile.js
@@ -1,16 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-
 router.get("/:userId", function (req, res) {
-    res.send('NOT IMPLEMENTED: User Profile functionality');
+  res.send("NOT IMPLEMENTED: User Profile functionality");
 });
 
 router.post("/userDetail/:userid", function (req, res) {
-    res.send('NOT IMPLEMENTED: User Profile update functionality');
+  res.send("NOT IMPLEMENTED: User Profile update functionality");
 });
 
-
-
-
-module.exports=router;
+module.exports = router;
