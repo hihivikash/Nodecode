bb41de2ff4891fa669554e45f33859f5fce600bb
Vikash <vikash.kumar@soprasteria.com>
2020-06-11 15:20:37 +0530

    added points in comment

--- app.js
+++ app.js
@@ -5,27 +5,26 @@ var bodyParser = require("body-parser");
 var AWS = require("aws-sdk");
 var app = express();
 
-require('dotenv/config');
+require("dotenv/config");
 
-var hotelProfileRoute = require('./routes/hotelprofile');
-var searchRoute = require('./routes/search');
-var bookingRoute = require('./routes/booking');
-var userProfileRoute = require('./routes/userProfile');
+var hotelProfileRoute = require("./routes/hotelprofile");
+var searchRoute = require("./routes/search");
+var bookingRoute = require("./routes/booking");
+var userProfileRoute = require("./routes/userProfile");
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-app.use('/hotel', hotelProfileRoute);
-app.use('/search', searchRoute);
-app.use('/book', bookingRoute);
-app.use('/user', userProfileRoute);
+app.use("/hotel", hotelProfileRoute);
+app.use("/search", searchRoute);
+app.use("/book", bookingRoute);
+app.use("/user", userProfileRoute);
 app.get("/", function (req, res) {
-  res.send({ title: "hotels API Entry Point" });
+  res.send({ title: "hotels API Entry Points" });
 });
 
-app.listen(process.env.PORT , () => console.log("hotels API listening on port 3000!"));
-
-
+app.listen(process.env.PORT, () =>
+  console.log("hotels API listening on port 3000!")
+);
